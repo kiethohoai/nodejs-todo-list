2b4b2747d1a2960c12711e3a930f9f4d23113367
kiethohoai <kietho.060193@gmail.com>
2024-07-04 14:07:46 +0700

    Bai-6-CRUD-command-nodejs

--- app/index.js
+++ app/index.js
@@ -8,13 +8,90 @@ yargs.command({
   },
 });
 
-// todo: CRUD
+// todo: CRUD COMMAND
+// node app/index.js create --title="Learning NodeJS"
+// node app/index.js create --title="Learning NodeJS" --desc="Fullstack Webs"
 yargs.command({
   command: "create",
-  handler: () => {
-    console.log("create");
+  builder: {
+    title: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  handler: (agrs) => {
+    const { title, desc } = agrs;
+    console.log("🚀CHECK  title =", title);
+    console.log("🚀CHECK  desc =", desc);
+    console.log(agrs);
   },
 });
+// node app/index.js read-all
+yargs.command({
+  command: "read-all",
+  handler: () => {
+    console.log("read-all");
+  },
+});
+// node app/index.js read-detail
+// node app/index.js read-detail --id="123456789"
+yargs.command({
+  command: "read-detail",
+  builder: {
+    id: {
+      type: "string",
+    },
+  },
+  handler: (agrs) => {
+    const { id } = agrs;
+    console.log("🚀CHECK  id =", id);
+  },
+});
+// node app/index.js update
+// node app/index.js update --id="123" --title="Learning NodeJS" --description="From Zero To Hero"
+yargs.command({
+  command: "update",
+  builder: {
+    id: {
+      type: "string",
+    },
+    title: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  handler: (agrs) => {
+    const { id, title, description } = agrs;
+    console.log("UPDATE");
+    console.log("🚀CHECK  id, title, description =", id, title, description);
+  },
+});
+// node app/index.js delete --id="123"
+yargs.command({
+  command: "delete",
+  builder: {
+    id: {
+      type: "string",
+    },
+  },
+  handler: (agrs) => {
+    const { id } = agrs;
+    console.log("delete");
+    console.log("🚀CHECK  id =", id);
+  },
+});
+
+/* 
+node app/index.js create
+node app/index.js read-all
+node app/index.js read-detail
+node app/index.js update
+node app/index.js delete
+ */
 
 // Save command after create
 yargs.parse();
