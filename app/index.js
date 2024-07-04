fb37b2f68567b476de01e1403cb66a446c73d17f
kiethohoai <kietho.060193@gmail.com>
2024-07-04 10:39:02 +0700

    Bai-5-Create-command-nodejs

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log("HELLO INDEX.JS WITH NODEMON");
+const yargs = require("yargs");
+
+// Create a command: node app/index.js test
+yargs.command({
+  command: "test",
+  handler: () => {
+    console.log("test");
+  },
+});
+
+// todo: CRUD
+yargs.command({
+  command: "create",
+  handler: () => {
+    console.log("create");
+  },
+});
+
+// Save command after create
+yargs.parse();
